3a9c7e58726a00d8ccbaa9135699c86381fbbb3e
林嘉鹏 <2602902282>
2024-10-10 10:11:18 +0800

    断点续传的方法续写

--- gduf-bilibili-service/src/main/java/com/gduf/bilibili/service/util/FastDFSUtil.java
+++ gduf-bilibili-service/src/main/java/com/gduf/bilibili/service/util/FastDFSUtil.java
@@ -3,15 +3,32 @@ package com.gduf.bilibili.service.util;
 import com.gduf.bilibili.exception.ConditionException;
 import com.github.tobato.fastdfs.domain.fdfs.MetaData;
 import com.github.tobato.fastdfs.domain.fdfs.StorePath;
+import com.github.tobato.fastdfs.service.AppendFileStorageClient;
 import com.github.tobato.fastdfs.service.FastFileStorageClient;
+import com.mysql.cj.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.multipart.MultipartFile;
-import java.util.HashSet;
-import java.util.Set;
+
+import java.util.*;
 
 public class FastDFSUtil {
     @Autowired
     private FastFileStorageClient fastFileStorageClient;
+    //用于断点续传的依赖
+    @Autowired
+    private AppendFileStorageClient appendFileStorageClient;
+
+    @Autowired
+    private RedisTemplate<String,String>redisTemplate;
+
+    private static final String DEFAULT_GROUP="group1";
+
+    private static final String PATH_KEY="path-key:";
+
+    private static final String UPLOAD_SIZE_KEY="upload-size-key:";
+
+    private static final String UPLOAD_NO_KEY="upload-no-key:";
 
     //获取文件类型
     public String getFileType(MultipartFile file){
@@ -30,6 +47,86 @@ public class FastDFSUtil {
         String fileType=this.getFileType(file);
         StorePath storePath = fastFileStorageClient.uploadFile(file.getInputStream(), file.getSize(), fileType, metaDataSet);
         return storePath.getPath();
+    }
+
+    //文件上传，支持断点续传
+    public String uploadAppenderFile(MultipartFile file) throws Exception {
+        //获取文件名主要是用来后续转为 md5 的，可以用来做秒传，避免重传
+        String filename = file.getOriginalFilename();
+        String fileType = this.getFileType(file);
+        StorePath storePath = appendFileStorageClient.uploadAppenderFile(DEFAULT_GROUP, file.getInputStream(), file.getSize(), fileType);
+        return storePath.getPath();
+    }
+
+    /**
+     * 该方法是用来支持断点续传的，原理是根据上传成功的第一个分片的路径，拼接接下来的二进制文件流
+     * @param file
+     * @param filePath  第一片文件路径
+     * @param offset  偏移量
+     * @throws Exception
+     */
+    public void modifyAppenderFile(MultipartFile file,String filePath,long offset) throws Exception {
+        appendFileStorageClient.modifyFile(DEFAULT_GROUP,filePath,file.getInputStream(),file.getSize(),offset);
+    }
+
+    public String uploadFileBySlices(MultipartFile file,String fileMd5,Integer sliceNo,Integer totalSliceNo) throws Exception {
+        if(file==null||sliceNo==null||totalSliceNo==null){
+            throw new ConditionException("参数异常");
+        }
+        //Redis中存储文件路径的键
+        String pathKey=PATH_KEY+fileMd5;
+        //Redis中存储文件总大小的键
+        String uploadSizeKey=UPLOAD_SIZE_KEY+fileMd5;
+        //Redis中存储文件分片是第几片的键
+        String uploadNoKey=UPLOAD_NO_KEY+fileMd5;
+        Long uploadSize=0L;
+        //从Redis中拿文件大小
+        String uploadSizeStr = redisTemplate.opsForValue().get(uploadSizeKey);
+        if(!StringUtils.isNullOrEmpty(uploadSizeStr)){
+            //如果Redis中文件大小存在，则需要对其进行初始化
+            uploadSize=Long.valueOf(uploadSizeStr);
+        }
+        String fileType=this.getFileType(file);
+        if(sliceNo==1){//前端上传的分片是第一片
+            String path = this.uploadAppenderFile(file);
+            if(StringUtils.isNullOrEmpty(path)){
+                throw new ConditionException("上传失败");
+            }
+            //上传成功的话就要修改Redis中的路径以及文件大小等
+            redisTemplate.opsForValue().set(pathKey,path);
+
+            //uploadSize+=file.getSize();
+            //redisTemplate.opsForValue().set(uploadSizeKey,String.valueOf(uploadSize));
+
+            //将已经上上传成功的片数存储到redis中
+            redisTemplate.opsForValue().set(uploadNoKey,"1");
+        }else {
+            String filePath = redisTemplate.opsForValue().get(pathKey);
+            if(StringUtils.isNullOrEmpty(filePath)){
+                throw new ConditionException("上传失败");
+            }
+            //偏移量就是Redis中存储的分片大小
+            this.modifyAppenderFile(file,filePath,uploadSize);
+            //上传成功后仍然需要对Redis中的数据进行更新
+            redisTemplate.opsForValue().increment(uploadNoKey);
+
+            //uploadSize+=file.getSize();
+            //redisTemplate.opsForValue().set(uploadSizeKey,String.valueOf(uploadSize));
+        }
+        uploadSize+=file.getSize();
+        redisTemplate.opsForValue().set(uploadSizeKey,String.valueOf(uploadSize));
+
+        //如果全部上传完毕的话，那么就要删除Redis中的key和value,并且返回给前端完整路径
+        String uploaNoStr = redisTemplate.opsForValue().get(uploadNoKey);
+        String resultPath="";
+        Integer uploadNo=Integer.valueOf(uploaNoStr);
+        if(uploadNo.equals(totalSliceNo)){
+            //上传完毕
+            resultPath=redisTemplate.opsForValue().get(pathKey);
+            List<String>keyList= Arrays.asList(pathKey,uploadSizeKey,uploadNoKey);
+            redisTemplate.delete(keyList);
+        }
+        return resultPath;
     }
 
     //删除
